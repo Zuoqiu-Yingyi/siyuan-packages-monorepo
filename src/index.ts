afa88448a34c0dd0724134a66893409590703159
颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2023-09-16 00:31:35 +0800

    feat(typewriter): 实现打字机模式基础功能 | Implement the basic functions of typewriter mode.

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,20 @@ import {
 } from "@workspace/utils/env/front-end";
 import { Logger } from "@workspace/utils/logger";
 import { mergeIgnoreArray } from "@workspace/utils/misc/merge";
+import { getEditors } from "@workspace/utils/siyuan/model";
+import { deshake } from "@workspace/utils/misc/deshake";
+import {
+    getCurrentBlock,
+    getCurrentProtyleWysiwyg,
+} from "@workspace/utils/siyuan/dom";
+
+import type {
+    IClickEditorContentEvent,
+    ILoadedProtyleEvent,
+    IDestroyProtyleEvent,
+} from "@workspace/types/siyuan/events";
+import type { IProtyle } from "siyuan/types/protyle";
+
 import { DEFAULT_CONFIG } from "./configs/default";
 import type { I18N } from "./utils/i18n";
 import type { IConfig } from "./types/config";
@@ -46,8 +60,11 @@ export default class TypewriterPlugin extends siyuan.Plugin {
     public readonly client: InstanceType<typeof Client>;
 
     protected readonly SETTINGS_DIALOG_ID: string;
+    protected readonly protyles = new WeakSet<IProtyle>();
 
     protected config: IConfig = DEFAULT_CONFIG;
+    protected scrollIntoView!: ReturnType<typeof deshake<(element: HTMLElement) => void>>;
+    protected currentElement?: Element; // 当前元素
 
     constructor(options: any) {
         super(options);
@@ -56,6 +73,7 @@ export default class TypewriterPlugin extends siyuan.Plugin {
         this.client = new Client(undefined, "fetch");
 
         this.SETTINGS_DIALOG_ID = `${this.name}-settings-dialog`;
+        this.updateScrollFunction();
     }
 
     onload(): void {
@@ -68,9 +86,11 @@ export default class TypewriterPlugin extends siyuan.Plugin {
         this.loadData(TypewriterPlugin.GLOBAL_CONFIG_NAME)
             .then(config => {
                 this.config = mergeIgnoreArray(DEFAULT_CONFIG, config || {}) as IConfig;
+                this.updateScrollFunction();
             })
             .catch(error => this.logger.error(error))
             .finally(() => {
+                this.activate(this.config.typewriter.enable);
             });
     }
 
@@ -78,6 +98,7 @@ export default class TypewriterPlugin extends siyuan.Plugin {
     }
 
     onunload(): void {
+        this.activate(false);
     }
 
     openSetting(): void {
@@ -110,6 +131,172 @@ export default class TypewriterPlugin extends siyuan.Plugin {
         if (config && config !== this.config) {
             this.config = config;
         }
+
+        this.activate(this.config.typewriter.enable);
+        this.updateScrollFunction();
+
         return this.saveData(TypewriterPlugin.GLOBAL_CONFIG_NAME, this.config);
     }
+
+    /**
+     * 切换监听器
+     * @param protyle 编辑器
+     * @param enable 是否启用打字机模式
+     */
+    protected toggleEventListener(
+        protyle: IProtyle,
+        enable: boolean,
+    ): void {
+        const listener = [
+            "keyup",
+            this.editorEventListener,
+            {
+                capture: true,
+            },
+        ] as Parameters<HTMLElement["addEventListener"]>;
+
+        switch (true) {
+            case enable && !this.protyles.has(protyle): // 未加入监听的编辑器
+                this.protyles.add(protyle);
+                protyle.wysiwyg?.element?.addEventListener(...listener);
+                break;
+
+            case !enable && this.protyles.has(protyle): // 已加入监听的编辑器
+                this.protyles.delete(protyle);
+                protyle.wysiwyg?.element?.removeEventListener(...listener);
+                break;
+        }
+    }
+
+    /**
+     * 激活或禁用打字机模式
+     * @param enable 是否启用打字机模式
+     */
+    protected activate(enable: boolean): void {
+        const editors = getEditors(); // 获取所有编辑器
+        for (const editor of editors) {
+            const protyle = editor?.protyle;
+            if (protyle) {
+                this.toggleEventListener(protyle, enable);
+            }
+        }
+
+        if (enable) {
+            this.eventBus.on("loaded-protyle", this.loadedProtyleEventListener);
+            this.eventBus.on("destroy-protyle", this.destroyProtyleEventListener);
+            this.eventBus.on("click-editorcontent", this.clickEditorContentEventListener);
+        }
+        else {
+            this.eventBus.off("loaded-protyle", this.loadedProtyleEventListener);
+            this.eventBus.off("destroy-protyle", this.destroyProtyleEventListener);
+            this.eventBus.off("click-editorcontent", this.clickEditorContentEventListener);
+        }
+    }
+
+    /**
+     * 更新滚动函数
+     * @param timeout 延时 (ms)
+     */
+    protected updateScrollFunction(timeout: number = this.config.typewriter.timeout): void {
+        this.scrollIntoView = deshake(
+            (element: HTMLElement) => {
+                this.logger.debug(element);
+                element.scrollIntoView({
+                    behavior: "smooth",
+                    inline: "center",
+                    block: "center",
+                });
+            },
+            timeout,
+        );
+    }
+
+    /* 编辑器加载事件 */
+    protected readonly loadedProtyleEventListener = (e: ILoadedProtyleEvent) => {
+        // this.logger.debug(e);
+
+        /* 若开启打字机模式, 添加编辑事件监听器 */
+        if (this.config.typewriter.enable) {
+            const protyle = e.detail;
+            this.toggleEventListener(protyle, true);
+        }
+    };
+
+    /* 编辑器关闭事件 */
+    protected readonly destroyProtyleEventListener = (e: IDestroyProtyleEvent) => {
+        // this.logger.debug(e);
+
+        /* 移除编辑事件监听器 */
+        const protyle = e.detail.protyle;
+        this.toggleEventListener(protyle, false);
+    };
+
+    /* 编辑器点击事件 */
+    protected readonly clickEditorContentEventListener = (e: IClickEditorContentEvent) => {
+        // this.logger.debug(e);
+
+        /* 若开启打字机模式, 派发编辑事件 */
+        if (this.config.typewriter.enable) {
+            const event = e.detail.event;
+            this.editorEventListener(event);
+        }
+    };
+
+    /* 编辑事件监听 */
+    protected readonly editorEventListener = (e: Event) => {
+        // this.logger.debug(e);
+
+        if (this.config.typewriter.enable) { // 已开启打字机模式
+            const block = getCurrentBlock(); // 当前光标所在块
+            if (block) {
+                let element = block;
+
+                switch (block.dataset.type) {
+                    case "NodeCodeBlock":
+                        if (this.config.typewriter.code.row) { // 定位到行
+
+                        }
+                        break;
+                    case "NodeTable":
+                        if (this.config.typewriter.table.row) { // 定位到行
+                            let focus = globalThis.getSelection()?.focusNode;
+                            while (true) {
+                                if (!focus) { // 元素不存在
+                                    break;
+                                }
+                                else { // 元素存在
+                                    if (focus instanceof HTMLElement) { // 元素为 HTML 元素
+                                        if (focus.localName === "td" || focus.localName === "th") { // 元素为表格单元格
+                                            break;
+                                        }
+                                    }
+                                }
+                                focus = focus.parentElement;
+                            }
+                            element = focus ?? block;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                if (this.currentElement === element) { // 当前元素未改变
+                    return;
+                }
+                else { // 更新当前元素并滚动
+                    this.currentElement = element;
+                    this.scrollIntoView(element);
+                }
+            }
+
+            // const page = getCurrentProtyleWysiwyg()?.parentElement; // 当前页面
+            // if (page?.classList.contains("protyle-content")) {
+
+            //     let block_height = block.clientHeight; // 当前块的高度
+            //     let block_bottom = block.getBoundingClientRect().bottom; // 当前块的底部
+            //     let page_height = page.clientHeight; // 当前页面的高度
+            //     let page_bottom = page.getBoundingClientRect().bottom; // 当前页面的底部
+            // }
+        }
+    };
 };
