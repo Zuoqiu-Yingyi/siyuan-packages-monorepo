71fcddd4caf93d64192c2e35fe97da7a46e05757
颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2023-09-17 00:39:17 +0800

    refactor(typewriter): 使用 `data-plugin-focus="true"` 替换 `class="plugin-focus-classname"`

--- src/constants.ts
+++ src/constants.ts
@@ -17,5 +17,6 @@
 
 export default {
     FOCUS_ELEMENT_UNIQUE_ID: "plugin-focus-unique-id", // 整个界面中焦点所在元素的 id 名称
-    FOCUS_ELEMENT_CLASS_NAME: "plugin-focus-classname", // 每个编辑器中焦点所在元素的 class 名称
+    FOCUS_ELEMENT_ATTR_NAME: "data-plugin-focus", // 每个编辑器中焦点所在元素的属性名称
+    FOCUS_ELEMENT_DATA_NAME: "pluginFocus", // 每个编辑器中焦点所在元素的数据名称
 };
