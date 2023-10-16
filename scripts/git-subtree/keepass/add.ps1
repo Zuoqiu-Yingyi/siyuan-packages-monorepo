6cc500bf107c07fbb94b35d01d08593f43cd6b14
颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2023-10-17 06:27:49 +0800

    chore(script): 添加附属仓库 `keepass` 的管理脚本 | Add management scripts for sub-repository `keepass`.

---
+++ scripts/git-subtree/keepass/add.ps1
@@ -0,0 +1,5 @@
+git subtree add `
+    -P workspace/plugins/keepass `
+    https://github.com/Zuoqiu-Yingyi/siyuan-plugin-keepass.git `
+    dev `
+    --squash
---
+++ scripts/git-subtree/keepass/pull.ps1
@@ -0,0 +1,4 @@
+git subtree pull `
+    -P workspace/plugins/keepass `
+    https://github.com/Zuoqiu-Yingyi/siyuan-plugin-keepass.git `
+    dev
