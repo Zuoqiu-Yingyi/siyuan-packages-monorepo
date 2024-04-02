cde49b4dac2a6f56c32681842686568e05831ea8
颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2024-04-02 22:27:42 +0800

    chore(wakatime): 重新添加附属仓库 `wakatime` | Re-add sub-repository `wakatime`.

--- workspace/plugins/wakatime/src/constants.ts
+++ workspace/plugins/wakatime/src/constants.ts
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 export default {
     OFFLINE_CACHE_PATH: "temp/.wakatime/cache", // 缓存文件目录路径
     WAKATIME_DEFAULT_API_URL: "https://wakatime.com/api/v1", // 默认 API 地址
@@ -21,7 +22,7 @@ export default {
     WAKATIME_HEARTBEATS_PATHNAME: "users/current/heartbeats", // 心跳数据 API 路径地址
     WAKATIME_HEARTBEATS_BULK: 25, // 每次提交数量限制
     WAKATIME_CLIENT_NAME: "wakatime", // 客户端名称
-    WAKATIME_CLIENT_VERSION: "1.86.0", // 客户端名称
+    WAKATIME_CLIENT_VERSION: "1.90.0", // 客户端版本
     WAKATIME_EDITOR_NAME: "siyuan", // 编辑器名称
     WAKATIME_PLUGIN_NAME: "siyuan-wakatime", // 插件名称
     WAKATIME_DEFAULT_LANGUAGE: "Siyuan", // 默认语言名称
