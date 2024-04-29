eef57b634b78a170f260b44e010b9caac9c86b9f
颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2024-04-29 13:14:52 +0800

    feat(monaco-editor): 实现嵌入块内容反转义功能 | Implement embedded block content unescape function.

---
+++ workspace/packages/types/utils/check.d.ts
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C) 2024 Zuoqiu Yingyi
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { A } from "ts-toolbelt";
+
+// REF https://github.com/millsp/ts-toolbelt/blob/master/tests/Any.ts
+export declare function check<T, U>(): A.Equals<T, U>;
+export declare function checks(...args: 1[]): void;
