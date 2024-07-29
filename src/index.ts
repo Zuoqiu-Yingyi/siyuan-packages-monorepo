6edfc36bb6eb08fde7f7c1cc454cb664be7f00ac
颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2024-07-29 22:42:02 +0800

    style(custom-fonts): 将 `eslint` 配置应用于 `custom-fonts` 模块 | Apply `eslint` configuration to `custom-fonts` module

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,52 @@
-/**
- * Copyright (C) 2023 Zuoqiu Yingyi
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-import siyuan from "siyuan";
+// Copyright (C) 2023 Zuoqiu Yingyi
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import { Client } from "@siyuan-community/siyuan-sdk";
+import siyuan from "siyuan";
+
+import List from "@workspace/components/siyuan/list/List.svelte";
 import {
     FLAG_MOBILE,
 } from "@workspace/utils/env/front-end";
+import { classify } from "@workspace/utils/font";
+import { fontData2CssFontStyle } from "@workspace/utils/font/css";
 import { Logger } from "@workspace/utils/logger";
 import { mergeIgnoreArray } from "@workspace/utils/misc/merge";
-import { classify } from "@workspace/utils/font";
-
-import { type IListItem } from "@workspace/components/siyuan/list/list";
-import List from "@workspace/components/siyuan/list/List.svelte"
-
-import { fontData2CssFontStyle } from "@workspace/utils/font/css";
 import { getBlockMenuContext } from "@workspace/utils/siyuan/menu/block";
 import { renderSnippets } from "@workspace/utils/siyuan/snippet";
 
 import Settings from "./components/Settings.svelte";
-
+import { DEFAULT_CONFIG } from "./configs/default";
 import {
     fontFamilyStyle,
 } from "./utils/style";
-import { DEFAULT_CONFIG } from "./configs/default";
 
-import type { ISiyuanGlobal } from "@workspace/types/siyuan";
+import type { IListItem } from "@workspace/components/siyuan/list/list";
+import type { FontData } from "@workspace/types/misc/browser";
 import type { IClickBlockIconEvent } from "@workspace/types/siyuan/events";
-import type { IConfig } from "./types/config";
+import type { Modify } from "@workspace/types/utils/readonly";
+
 import type { I18N } from "@/utils/i18n";
 
-declare var globalThis: ISiyuanGlobal;
+import type { IConfig } from "./types/config";
+
+declare global {
+    interface Window {
+        queryLocalFonts: () => Promise<FontData[]>;
+    }
+}
 
 export default class CustomFontsPlugin extends siyuan.Plugin {
     static readonly GLOBAL_CONFIG_NAME = "global-config";
@@ -74,7 +76,7 @@ export default class CustomFontsPlugin extends siyuan.Plugin {
         this.USABLE_FONTS_DIALOG_ID = `plugin-${this.name}-usable-fonts-dialog`;
     }
 
-    onload(): void {
+    public override onload(): void {
         /**
          * 注册快捷键命令
          * 在 onload 结束后即刻解析, 因此不能在回调函数中注册
@@ -94,10 +96,10 @@ export default class CustomFontsPlugin extends siyuan.Plugin {
 
         /* 加载数据 */
         this.loadData(CustomFontsPlugin.GLOBAL_CONFIG_NAME)
-            .then(config => {
+            .then((config) => {
                 this.config = mergeIgnoreArray(DEFAULT_CONFIG, config || {}) as IConfig;
             })
-            .catch(error => this.logger.error(error))
+            .catch((error) => this.logger.error(error))
             .finally(async () => {
                 await this.updateStyle();
 
@@ -105,20 +107,20 @@ export default class CustomFontsPlugin extends siyuan.Plugin {
             });
     }
 
-    onLayoutReady(): void {
+    public override onLayoutReady(): void {
     }
 
-    onunload(): void {
+    public override onunload(): void {
         this.eventBus.off("click-blockicon", this.blockMenuEventListener);
 
         this.client.getSnippet({
             type: "all",
             enabled: 2,
-        }).then(response => {
+        }).then((response) => {
             const snippets = response.data.snippets;
-            const snippet = snippets.find(s => s.name === this.STYLE_SNIPPET_NAME && s.type === "css");
+            const snippet = snippets.find((s) => s.name === this.STYLE_SNIPPET_NAME && s.type === "css");
             if (snippet) {
-                snippet.enabled = false;
+                (snippet as Modify<typeof snippet>).enabled = false;
                 this.client.setSnippet({
                     snippets,
                 });
@@ -126,22 +128,24 @@ export default class CustomFontsPlugin extends siyuan.Plugin {
         });
     }
 
-    openSetting(): void {
-        const that = this;
+    public override openSetting(): void {
+        // eslint-disable-next-line ts/no-this-alias
+        const plugin = this;
         const dialog = new siyuan.Dialog({
             title: `${this.i18n.displayName} <code class="fn__code">${this.name}</code>`,
-            content: `<div id="${that.SETTINGS_DIALOG_ID}" class="fn__flex-column" />`,
+            content: `<div id="${plugin.SETTINGS_DIALOG_ID}" class="fn__flex-column" />`,
             width: FLAG_MOBILE ? "92vw" : "960px",
             height: FLAG_MOBILE ? undefined : "720px",
         });
         const settings = new Settings({
-            target: dialog.element.querySelector(`#${that.SETTINGS_DIALOG_ID}`)!,
+            target: dialog.element.querySelector(`#${plugin.SETTINGS_DIALOG_ID}`)!,
             props: {
                 config: this.config,
                 plugin: this,
                 textareaHeight: 320,
             },
         });
+        void settings;
     }
 
     /* 更新样式 */
@@ -151,7 +155,7 @@ export default class CustomFontsPlugin extends siyuan.Plugin {
             enabled: 2,
         });
         const snippets = response.data.snippets;
-        var snippet = snippets.find(s => s.name === this.STYLE_SNIPPET_NAME && s.type === "css")
+        let snippet = snippets.find((s) => s.name === this.STYLE_SNIPPET_NAME && s.type === "css");
         const content = [
             this.config.css.enable ? this.config.css.code : undefined,
             fontFamilyStyle({
@@ -165,12 +169,12 @@ export default class CustomFontsPlugin extends siyuan.Plugin {
         ].join("\n\n");
 
         if (snippet) {
-            snippet.enabled = true;
-            snippet.content = content;
+            (snippet as Modify<typeof snippet>).enabled = true;
+            (snippet as Modify<typeof snippet>).content = content;
         }
         else {
             snippet = {
-                id: globalThis.Lute.NewNodeID(),
+                id: window.Lute.NewNodeID(),
                 type: "css",
                 name: this.STYLE_SNIPPET_NAME,
                 memo: "",
@@ -230,26 +234,27 @@ export default class CustomFontsPlugin extends siyuan.Plugin {
                             items: fonts,
                         },
                     });
+                    void list;
                 }
                 else { // 请求失败
                     dialog_body.textContent = response.msg; // 显示响应信息
                 }
-            }
+            },
         );
     }
+
     /* 显示当前可用的字体列表 */
     public async showUsableFonts(): Promise<void> {
         // REF https://developer.mozilla.org/en-US/docs/Web/API/Window/queryLocalFonts
         if ("queryLocalFonts" in globalThis) { // 当前应用支持查询本地字体
-            // @ts-ignore
-            const availableFonts: FontData[] = await globalThis.queryLocalFonts(); // 本地字体列表
+            const availableFonts = await window.queryLocalFonts(); // 本地字体列表
             const fonts: IListItem[] = []; // 待显示的字体列表
 
             const classified_fonts = classify(availableFonts);
-            classified_fonts.families.forEach(family => {
+            classified_fonts.families.forEach((family) => {
                 const font_list = classified_fonts.map.get(family);
                 if (font_list) {
-                    const reaular_font = font_list.find(font => font.style === "Regular") ?? font_list[0];
+                    const reaular_font = font_list.find((font) => font.style === "Regular") ?? font_list[0]!;
                     fonts.push({
                         icon: "#iconFont",
                         text: reaular_font.fullName,
@@ -257,7 +262,7 @@ export default class CustomFontsPlugin extends siyuan.Plugin {
                         style: `font-family: "${reaular_font.family}"`,
                         fold: true,
                         indent: "18px",
-                        children: font_list.map(font => ({
+                        children: font_list.map((font) => ({
                             icon: "#iconFont",
                             text: font.fullName,
                             meta: font.style,
@@ -279,6 +284,7 @@ export default class CustomFontsPlugin extends siyuan.Plugin {
                     items: fonts,
                 },
             });
+            void list;
         }
         else { // 当前应用不支持查询本地字体
             siyuan.showMessage(`${this.i18n.message.notSupportQueryLocalFonts}<br />[${this.i18n.displayName} <code class="fn__code">${this.name}</code>]`, undefined, "error");
@@ -298,7 +304,7 @@ export default class CustomFontsPlugin extends siyuan.Plugin {
                 icon: "iconClear",
                 label: this.i18n.menu.clearFontStyle.label,
                 click: () => {
-                    context.blocks.forEach(async block => {
+                    context.blocks.forEach(async (block) => {
                         block.element.style.fontFamily = "";
                         const style = block.element.getAttribute("style");
                         this.client.setBlockAttrs({
@@ -316,8 +322,9 @@ export default class CustomFontsPlugin extends siyuan.Plugin {
             });
 
             /* 设置字体 */
-            this.config.menu.block.list.forEach(font => {
-                if (/^\s*$/.test(font)) return;
+            this.config.menu.block.list.forEach((font) => {
+                if (/^\s*$/.test(font))
+                    return;
                 else font = font.trim();
 
                 submenu.push({
@@ -325,7 +332,7 @@ export default class CustomFontsPlugin extends siyuan.Plugin {
                     label: font,
                     accelerator: `font-family: ${font}`,
                     click: () => {
-                        context.blocks.forEach(async block => {
+                        context.blocks.forEach(async (block) => {
                             block.element.style.fontFamily = font;
                             const style = block.element.getAttribute("style");
                             this.client.setBlockAttrs({
@@ -336,7 +343,7 @@ export default class CustomFontsPlugin extends siyuan.Plugin {
                             });
                         });
                     },
-                    bind: element => {
+                    bind: (element) => {
                         const label = element.querySelector<HTMLElement>(".b3-menu__label");
                         if (label) {
                             label.style.fontFamily = font;
@@ -351,5 +358,5 @@ export default class CustomFontsPlugin extends siyuan.Plugin {
                 submenu,
             });
         }
-    }
+    };
 };
