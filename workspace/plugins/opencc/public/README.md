75ec715dbdb8a13c4472bcc55c0deb9bbc96dea8
颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2024-09-17 16:46:05 +0800

    style(opencc): 将 `eslint` 配置应用于 `opencc` 模块 | Apply `eslint` configuration to `opencc` module

--- workspace/plugins/opencc/public/README.md
+++ workspace/plugins/opencc/public/README.md
@@ -10,11 +10,12 @@
 ![hits](https://hits.b3log.org/Zuoqiu-Yingyi/siyuan-plugin-opencc.svg)
 <!-- ![jsDelivr hits (GitHub)](https://img.shields.io/jsdelivr/gh/hy/Zuoqiu-Yingyi/siyuan-packages-opencc?style=flat-square) -->
 [![GitHub all releases](https://img.shields.io/github/downloads/Zuoqiu-Yingyi/siyuan-plugin-opencc/total?style=flat-square)](https://github.com/Zuoqiu-Yingyi/siyuan-plugin-opencc/releases)
-
 ---
+
 [简体中文](./README_zh_CN.md) \| [繁体中文](./README_zh_CHT.md) \| English
 
 ---
+
 </div>
 
 # SiYuan Plugin for Chinese Convert
@@ -31,128 +32,128 @@ This is a plugin for [Siyuan Note](https://github.com/siyuan-note/siyuan) that p
 
 ### Function Introduction
 
-* Supported text types
+- Supported text types
 
-  * `zh-Hans-CN` Simplified Chinese
-  * `zh-Hant` Traditional Chinese (OpenCC standard)
-  * `zh-Hant-HK` Traditional Chinese (Hong Kong)
-  * `zh-Hant-TW` Traditional Chinese (Taiwan)
-  * `zh-Hant-TW` Traditional Chinese (Taiwan Common Words)
-  * `ja-Hani-JP` Japanese New Font
-  * For more details, please refer to the [OpenCC preset configuration file](https://github.com/BYVoid/OpenCC#%E9%A0%90%E8%A8%AD%E9%85%8D%E7%BD%AE%E6%96%87%E4%BB%B6)
-* Use the <kbd>Right-click menu</kbd>/<kbd>Block menu</kbd> > <kbd>Plugin</kbd> > <kbd>Chinese Convert</kbd> menu item to invoke this plugin's functions.
+  - `zh-Hans-CN` Simplified Chinese
+  - `zh-Hant` Traditional Chinese (OpenCC standard)
+  - `zh-Hant-HK` Traditional Chinese (Hong Kong)
+  - `zh-Hant-TW` Traditional Chinese (Taiwan)
+  - `zh-Hant-TW` Traditional Chinese (Taiwan Common Words)
+  - `ja-Hani-JP` Japanese New Font
+  - For more details, please refer to the [OpenCC preset configuration file](https://github.com/BYVoid/OpenCC#%E9%A0%90%E8%A8%AD%E9%85%8D%E7%BD%AE%E6%96%87%E4%BB%B6)
+- Use the <kbd>Right-click menu</kbd>/<kbd>Block menu</kbd> > <kbd>Plugin</kbd> > <kbd>Chinese Convert</kbd> menu item to invoke this plugin's functions.
 
-  * `Simplified → Traditional`
+  - `Simplified → Traditional`
 
-    * The types of simplified and traditional characters can be set in the `Plugin Settings > Conversion Settings > Simplified→Traditional` page.
-  * `Traditional → Simplified`
+    - The types of simplified and traditional characters can be set in the `Plugin Settings > Conversion Settings > Simplified→Traditional` page.
+  - `Traditional → Simplified`
 
-    * The types of simplified and traditional characters can be set in the `Plugin Settings > Conversion Settings > Traditional→Simplified` page.
-  * `Custom Conversion`
+    - The types of simplified and traditional characters can be set in the `Plugin Settings > Conversion Settings > Traditional→Simplified` page.
+  - `Custom Conversion`
 
-    * The types of original and target text can be set in the `Plugin Settings > Conversion Settings > Custom Conversion` page.
-  * `Other Conversions`
+    - The types of original and target text can be set in the `Plugin Settings > Conversion Settings > Custom Conversion` page.
+  - `Other Conversions`
 
-    * Temporarily call a specific type of conversion.
-* Menu Item Introduction
+    - Temporarily call a specific type of conversion.
+- Menu Item Introduction
 
-  * `Copy Conversion Result`
+  - `Copy Conversion Result`
 
-    * Copy the conversion result of the selected content to the clipboard.
-  * `Insert Conversion Result Above`
+    - Copy the conversion result of the selected content to the clipboard.
+  - `Insert Conversion Result Above`
 
-    * Insert the conversion result above the selected content.
-  * `Insert Conversion Result Above Each Block`
+    - Insert the conversion result above the selected content.
+  - `Insert Conversion Result Above Each Block`
 
-    * Insert the conversion result of each leaf block above the corresponding block in the selected block.
-  * `Replace Original Text with Conversion Result`
+    - Insert the conversion result of each leaf block above the corresponding block in the selected block.
+  - `Replace Original Text with Conversion Result`
 
-    * Replace the selected content with the conversion result.
-  * `Insert Conversion Result Below Each Block`
+    - Replace the selected content with the conversion result.
+  - `Insert Conversion Result Below Each Block`
 
-    * Insert the conversion result of each leaf block below the corresponding block in the selected block.
-  * `Insert Conversion Result Below`
+    - Insert the conversion result of each leaf block below the corresponding block in the selected block.
+  - `Insert Conversion Result Below`
 
-    * Insert the conversion result below the selected content.
-  * `Convert Document Title`
+    - Insert the conversion result below the selected content.
+  - `Convert Document Title`
 
-    * Rename the document using the converted document title.
+    - Rename the document using the converted document title.
 
 ### Setting Item Introduction
 
-* `General Settings`
+- `General Settings`
 
-  * `Reset Settings Options`
+  - `Reset Settings Options`
 
-    * Reset all settings options to their default values.
-    * After clicking this button, a confirmation dialog will pop up.
+    - Reset all settings options to their default values.
+    - After clicking this button, a confirmation dialog will pop up.
 
-      * After clicking the confirmation button in the dialog, all options of this plugin will be reset to their default values, and the current interface will be automatically refreshed.
-* `Conversion Settings`
+      - After clicking the confirmation button in the dialog, all options of this plugin will be reset to their default values, and the current interface will be automatically refreshed.
+- `Conversion Settings`
 
-  * `Global Settings`
+  - `Global Settings`
 
-    * `Global Extension Dictionary`
+    - `Global Extension Dictionary`
 
-      * A user-defined dictionary that takes effect globally. It works in operations such as Simplified → Traditional, Traditional → Simplified, Custom Conversion, and Other Conversions.
-      * Dictionary format
+      - A user-defined dictionary that takes effect globally. It works in operations such as Simplified → Traditional, Traditional → Simplified, Custom Conversion, and Other Conversions.
+      - Dictionary format
 
-        * The original words and target words are separated by spaces in each entry, and the entries are separated by the | character or newline character, as shown below
-        * ```plaintext
+        - The original words and target words are separated by spaces in each entry, and the entries are separated by the | character or newline character, as shown below
+        - ```plaintext
           原始词汇1 目标词汇1|原始词汇2 目标词汇2|原始词汇3 目标词汇3
           原始词汇4 目标词汇4|原始词汇5 目标词汇5|原始词汇6 目标词汇6
           ```
-  * `Simplified → Traditional`
+  - `Simplified → Traditional`
 
-    * `Original Text Type`
+    - `Original Text Type`
 
-      * The text type of the selected content when using the Simplified → Traditional menu item. The following text types can be selected.
+      - The text type of the selected content when using the Simplified → Traditional menu item. The following text types can be selected.
 
-        * zh-Hans-CN Simplified Chinese
-        * ja-Hani-JP Japanese New Font
-    * `Target Text Type`
+        - zh-Hans-CN Simplified Chinese
+        - ja-Hani-JP Japanese New Font
+    - `Target Text Type`
 
-      * The text type to convert to when using the Simplified → Traditional menu item. The following text types can be selected.
+      - The text type to convert to when using the Simplified → Traditional menu item. The following text types can be selected.
 
-        * zh-Hant Traditional Chinese (OpenCC standard)
-        * zh-Hant-HK Traditional Chinese (Hong Kong)
-        * zh-Hant-TW Traditional Chinese (Taiwan)
-        * zh-Hant-TW Traditional Chinese (Taiwan Common Words)
-        * ja-Hani-JP Japanese New Font
-    * `Extension Dictionary`
+        - zh-Hant Traditional Chinese (OpenCC standard)
+        - zh-Hant-HK Traditional Chinese (Hong Kong)
+        - zh-Hant-TW Traditional Chinese (Taiwan)
+        - zh-Hant-TW Traditional Chinese (Taiwan Common Words)
+        - ja-Hani-JP Japanese New Font
+    - `Extension Dictionary`
 
-      * A user-defined dictionary that only takes effect when using the Simplified → Traditional menu item. The dictionary format is the same as the Global Extension Dictionary.
-  * `Traditional → Simplified`
+      - A user-defined dictionary that only takes effect when using the Simplified → Traditional menu item. The dictionary format is the same as the Global Extension Dictionary.
+  - `Traditional → Simplified`
 
-    * `Original Text Type`
+    - `Original Text Type`
 
-      * The text type of the selected content when using the Traditional → Simplified menu item. The following text types can be selected.
+      - The text type of the selected content when using the Traditional → Simplified menu item. The following text types can be selected.
 
-        * zh-Hant Traditional Chinese (OpenCC standard)
-        * zh-Hant-HK Traditional Chinese (Hong Kong)
-        * zh-Hant-TW Traditional Chinese (Taiwan)
-        * zh-Hant-TW Traditional Chinese (Taiwan Common Words)
-        * ja-Hani-JP Japanese New Font
-    * `Target Text Type`
+        - zh-Hant Traditional Chinese (OpenCC standard)
+        - zh-Hant-HK Traditional Chinese (Hong Kong)
+        - zh-Hant-TW Traditional Chinese (Taiwan)
+        - zh-Hant-TW Traditional Chinese (Taiwan Common Words)
+        - ja-Hani-JP Japanese New Font
+    - `Target Text Type`
 
-      * The text type to convert to when using the Traditional → Simplified menu item. The following text types can be selected.
+      - The text type to convert to when using the Traditional → Simplified menu item. The following text types can be selected.
 
-        * zh-Hans-CN Simplified Chinese
-        * ja-Hani-JP Japanese New Font
-    * `Extension Dictionary`
+        - zh-Hans-CN Simplified Chinese
+        - ja-Hani-JP Japanese New Font
+    - `Extension Dictionary`
 
-      * A user-defined dictionary that only takes effect when using the Traditional → Simplified menu item. The dictionary format is the same as the Global Extension Dictionary.
-  * `Custom Conversion`
+      - A user-defined dictionary that only takes effect when using the Traditional → Simplified menu item. The dictionary format is the same as the Global Extension Dictionary.
+  - `Custom Conversion`
 
-    * `Original Text Type`
+    - `Original Text Type`
 
-      * The text type of the selected content when using the Custom Conversion menu item. All text types are supported.
-    * `Target Text Type`
+      - The text type of the selected content when using the Custom Conversion menu item. All text types are supported.
+    - `Target Text Type`
 
-      * The text type to convert to when using the Custom Conversion menu item. All text types are supported.
-    * `Extension Dictionary`
+      - The text type to convert to when using the Custom Conversion menu item. All text types are supported.
+    - `Extension Dictionary`
 
-      * A user-defined dictionary that only takes effect when using the Custom Conversion menu item. The dictionary format is the same as the Global Extension Dictionary.
+      - A user-defined dictionary that only takes effect when using the Custom Conversion menu item. The dictionary format is the same as the Global Extension Dictionary.
 
 ## CHANGELOG
 
--- workspace/plugins/opencc/src/components/Settings.svelte
+++ workspace/plugins/opencc/src/components/Settings.svelte
@@ -1,16 +1,16 @@
 <!--
  Copyright (C) 2023 Zuoqiu Yingyi
- 
+
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU Affero General Public License as
  published by the Free Software Foundation, either version 3 of the
  License, or (at your option) any later version.
- 
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU Affero General Public License for more details.
- 
+
  You should have received a copy of the GNU Affero General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -->
@@ -18,22 +18,21 @@
 <!-- 设置面板 -->
 
 <script lang="ts">
-    import Panels from "@workspace/components/siyuan/setting/panel/Panels.svelte";
-    import Panel from "@workspace/components/siyuan/setting/panel/Panel.svelte";
-    import Tabs from "@workspace/components/siyuan/setting/tab/Tabs.svelte";
-    import Item from "@workspace/components/siyuan/setting/item/Item.svelte";
     import Input from "@workspace/components/siyuan/setting/item/Input.svelte";
-
     import { ItemType } from "@workspace/components/siyuan/setting/item/item";
+    import Item from "@workspace/components/siyuan/setting/item/Item.svelte";
+    import Panel from "@workspace/components/siyuan/setting/panel/Panel.svelte";
+    import Panels from "@workspace/components/siyuan/setting/panel/Panels.svelte";
     import { type ITab } from "@workspace/components/siyuan/setting/tab";
-
-    import type Plugin from "@/index";
-
-    import type { IConfig } from "@/types/config";
-    import type { I18N } from "@/utils/i18n";
+    import Tabs from "@workspace/components/siyuan/setting/tab/Tabs.svelte";
 
     import { Locale } from "@/opencc";
+
     import { locale2lang } from "../opencc/lang";
+
+    import type Plugin from "@/index";
+    import type { IConfig } from "@/types/config";
+    import type { I18N } from "@/utils/i18n";
 
     export let config: IConfig; // 传入的配置项
     export let plugin: InstanceType<typeof Plugin>; // 插件实例
@@ -55,6 +54,7 @@
         );
     }
 
+    /* eslint-disable no-unused-vars */
     enum PanelKey {
         general, // 常规设置
         convert, // 转换设置
@@ -66,8 +66,9 @@
         t2s, // 繁体 -> 简体
         custom, // 自定义
     }
+    /* eslint-enable no-unused-vars */
 
-    let panels_focus_key = PanelKey.general;
+    const panels_focus_key = PanelKey.general;
     const panels: ITab[] = [
         {
             key: PanelKey.general,
@@ -83,7 +84,7 @@
         },
     ];
 
-    let convert_settings_tabs_focus_key = TabKey.global;
+    const convert_settings_tabs_focus_key = TabKey.global;
     const tabs = {
         convert: [
             {
@@ -122,44 +123,44 @@
 
     /* 简体类型标签 */
     const simplified_options = [
-        full_options[0], // cn
-        full_options[5], // jp
+        full_options[0]!, // cn
+        full_options[5]!, // jp
     ];
 
     /* 繁体类型标签 */
     const traditional_options = [
-        full_options[1], // t
-        full_options[2], // hk
-        full_options[3], // tw
-        full_options[4], // twp
-        full_options[5], // jp
+        full_options[1]!, // t
+        full_options[2]!, // hk
+        full_options[3]!, // tw
+        full_options[4]!, // twp
+        full_options[5]!, // jp
     ];
 </script>
 
 <Panels
-    {panels}
     focus={panels_focus_key}
+    {panels}
     let:focus={focusPanel}
 >
     <!-- 常规设置面板 -->
-    <Panel display={panels[0].key === focusPanel}>
+    <Panel display={panels[0]?.key === focusPanel}>
         <!-- 重置设置 -->
         <Item
-            title={i18n.settings.generalSettings.reset.title}
             text={i18n.settings.generalSettings.reset.description}
+            title={i18n.settings.generalSettings.reset.title}
         >
             <Input
                 slot="input"
-                type={ItemType.button}
                 settingKey="Reset"
                 settingValue={i18n.settings.generalSettings.reset.text}
+                type={ItemType.button}
                 on:clicked={resetOptions}
             />
         </Item>
     </Panel>
 
     <!-- 转换设置面板 -->
-    <Panel display={panels[1].key === focusPanel}>
+    <Panel display={panels[1]?.key === focusPanel}>
         <Tabs
             focus={convert_settings_tabs_focus_key}
             tabs={tabs.convert}
@@ -167,23 +168,23 @@
         >
             <!-- 标签页 1 - 全局设置 -->
             <div
-                data-type={tabs.convert[0].name}
-                class:fn__none={tabs.convert[0].key !== focusTab}
+                class:fn__none={tabs.convert[0]?.key !== focusTab}
+                data-type={tabs.convert[0]?.name}
             >
                 <!-- 全局扩展词典 -->
                 <Item
-                    title={i18n.settings.convertSettings.globalTab.dictionary.title}
-                    text={i18n.settings.options.dictionary.description}
                     block={true}
+                    text={i18n.settings.options.dictionary.description}
+                    title={i18n.settings.convertSettings.globalTab.dictionary.title}
                 >
                     <Input
                         slot="input"
-                        type={ItemType.textarea}
+                        block={true}
+                        placeholder={i18n.settings.options.dictionary.placeholder}
                         settingKey="dictionary"
                         settingValue={config.opencc.dict}
-                        placeholder={i18n.settings.options.dictionary.placeholder}
-                        block={true}
-                        on:changed={async e => {
+                        type={ItemType.textarea}
+                        on:changed={async (e) => {
                             config.opencc.dict = e.detail.value;
                             await updated();
                         }}
@@ -193,21 +194,21 @@
 
             <!-- 标签页 2 - 简→繁 -->
             <div
-                data-type={tabs.convert[1].name}
-                class:fn__none={tabs.convert[1].key !== focusTab}
+                class:fn__none={tabs.convert[1]?.key !== focusTab}
+                data-type={tabs.convert[1]?.name}
             >
                 <!-- 原始文本类型 -->
                 <Item
-                    title={i18n.settings.options.sourceTextType.title}
                     text={i18n.settings.options.sourceTextType.description}
+                    title={i18n.settings.options.sourceTextType.title}
                 >
                     <Input
                         slot="input"
-                        type={ItemType.select}
+                        options={simplified_options}
                         settingKey="s2t.from"
                         settingValue={config.opencc.s2t.from}
-                        options={simplified_options}
-                        on:changed={async e => {
+                        type={ItemType.select}
+                        on:changed={async (e) => {
                             config.opencc.s2t.from = e.detail.value;
                             await updated();
                         }}
@@ -216,16 +217,16 @@
 
                 <!-- 目标文本类型 -->
                 <Item
-                    title={i18n.settings.options.targetTextType.title}
                     text={i18n.settings.options.targetTextType.description}
+                    title={i18n.settings.options.targetTextType.title}
                 >
                     <Input
                         slot="input"
-                        type={ItemType.select}
+                        options={traditional_options}
                         settingKey="s2t.to"
                         settingValue={config.opencc.s2t.to}
-                        options={traditional_options}
-                        on:changed={async e => {
+                        type={ItemType.select}
+                        on:changed={async (e) => {
                             config.opencc.s2t.to = e.detail.value;
                             await updated();
                         }}
@@ -234,18 +235,18 @@
 
                 <!-- 扩展词典 -->
                 <Item
-                    title={i18n.settings.options.dictionary.title}
-                    text={i18n.settings.options.dictionary.description}
                     block={true}
+                    text={i18n.settings.options.dictionary.description}
+                    title={i18n.settings.options.dictionary.title}
                 >
                     <Input
                         slot="input"
-                        type={ItemType.textarea}
+                        block={true}
+                        placeholder={i18n.settings.options.dictionary.placeholder}
                         settingKey="s2t.dictionary"
                         settingValue={config.opencc.s2t.dict}
-                        placeholder={i18n.settings.options.dictionary.placeholder}
-                        block={true}
-                        on:changed={async e => {
+                        type={ItemType.textarea}
+                        on:changed={async (e) => {
                             config.opencc.s2t.dict = e.detail.value;
                             await updated();
                         }}
@@ -255,21 +256,21 @@
 
             <!-- 标签页 3 - 繁→简 -->
             <div
-                data-type={tabs.convert[2].name}
-                class:fn__none={tabs.convert[2].key !== focusTab}
+                class:fn__none={tabs.convert[2]?.key !== focusTab}
+                data-type={tabs.convert[2]?.name}
             >
                 <!-- 原始文本类型 -->
                 <Item
-                    title={i18n.settings.options.sourceTextType.title}
                     text={i18n.settings.options.sourceTextType.description}
+                    title={i18n.settings.options.sourceTextType.title}
                 >
                     <Input
                         slot="input"
-                        type={ItemType.select}
+                        options={traditional_options}
                         settingKey="t2s.from"
                         settingValue={config.opencc.t2s.from}
-                        options={traditional_options}
-                        on:changed={async e => {
+                        type={ItemType.select}
+                        on:changed={async (e) => {
                             config.opencc.t2s.from = e.detail.value;
                             await updated();
                         }}
@@ -278,16 +279,16 @@
 
                 <!-- 目标文本类型 -->
                 <Item
-                    title={i18n.settings.options.targetTextType.title}
                     text={i18n.settings.options.targetTextType.description}
+                    title={i18n.settings.options.targetTextType.title}
                 >
                     <Input
                         slot="input"
-                        type={ItemType.select}
+                        options={simplified_options}
                         settingKey="t2s.to"
                         settingValue={config.opencc.t2s.to}
-                        options={simplified_options}
-                        on:changed={async e => {
+                        type={ItemType.select}
+                        on:changed={async (e) => {
                             config.opencc.t2s.to = e.detail.value;
                             await updated();
                         }}
@@ -296,18 +297,18 @@
 
                 <!-- 扩展词典 -->
                 <Item
-                    title={i18n.settings.options.dictionary.title}
-                    text={i18n.settings.options.dictionary.description}
                     block={true}
+                    text={i18n.settings.options.dictionary.description}
+                    title={i18n.settings.options.dictionary.title}
                 >
                     <Input
                         slot="input"
-                        type={ItemType.textarea}
+                        block={true}
+                        placeholder={i18n.settings.options.dictionary.placeholder}
                         settingKey="t2s.dictionary"
                         settingValue={config.opencc.t2s.dict}
-                        placeholder={i18n.settings.options.dictionary.placeholder}
-                        block={true}
-                        on:changed={async e => {
+                        type={ItemType.textarea}
+                        on:changed={async (e) => {
                             config.opencc.t2s.dict = e.detail.value;
                             await updated();
                         }}
@@ -317,21 +318,21 @@
 
             <!-- 标签页 4 - 自定义转换 -->
             <div
-                data-type={tabs.convert[3].name}
-                class:fn__none={tabs.convert[3].key !== focusTab}
+                class:fn__none={tabs.convert[3]?.key !== focusTab}
+                data-type={tabs.convert[3]?.name}
             >
                 <!-- 原始文本类型 -->
                 <Item
-                    title={i18n.settings.options.sourceTextType.title}
                     text={i18n.settings.options.sourceTextType.description}
+                    title={i18n.settings.options.sourceTextType.title}
                 >
                     <Input
                         slot="input"
-                        type={ItemType.select}
+                        options={full_options}
                         settingKey="custom.from"
                         settingValue={config.opencc.custom.from}
-                        options={full_options}
-                        on:changed={async e => {
+                        type={ItemType.select}
+                        on:changed={async (e) => {
                             config.opencc.custom.from = e.detail.value;
                             await updated();
                         }}
@@ -340,16 +341,16 @@
 
                 <!-- 目标文本类型 -->
                 <Item
-                    title={i18n.settings.options.targetTextType.title}
                     text={i18n.settings.options.targetTextType.description}
+                    title={i18n.settings.options.targetTextType.title}
                 >
                     <Input
                         slot="input"
-                        type={ItemType.select}
+                        options={full_options}
                         settingKey="custom.to"
                         settingValue={config.opencc.custom.to}
-                        options={full_options}
-                        on:changed={async e => {
+                        type={ItemType.select}
+                        on:changed={async (e) => {
                             config.opencc.custom.to = e.detail.value;
                             await updated();
                         }}
@@ -358,18 +359,18 @@
 
                 <!-- 扩展词典 -->
                 <Item
-                    title={i18n.settings.options.dictionary.title}
-                    text={i18n.settings.options.dictionary.description}
                     block={true}
+                    text={i18n.settings.options.dictionary.description}
+                    title={i18n.settings.options.dictionary.title}
                 >
                     <Input
                         slot="input"
-                        type={ItemType.textarea}
+                        block={true}
+                        placeholder={i18n.settings.options.dictionary.placeholder}
                         settingKey="custom.dictionary"
                         settingValue={config.opencc.custom.dict}
-                        placeholder={i18n.settings.options.dictionary.placeholder}
-                        block={true}
-                        on:changed={async e => {
+                        type={ItemType.textarea}
+                        on:changed={async (e) => {
                             config.opencc.custom.dict = e.detail.value;
                             await updated();
                         }}
